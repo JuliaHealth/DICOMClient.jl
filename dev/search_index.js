463db61ff6a447bbc02ebb21010ada88dd9bdca8
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-08-03 15:24:05 +0000

    build based on de216f0

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DICOMClient","category":"page"},{"location":"#DICOMClient","page":"Home","title":"DICOMClient","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DICOMClient]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DICOMClient","category":"page"},{"location":"#DICOMClient","page":"Home","title":"DICOMClient","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [DICOMClient]","category":"page"}]
 }
