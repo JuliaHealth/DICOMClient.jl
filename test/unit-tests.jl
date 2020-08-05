8020d66585e3aabaed35311f6316267572e7ecda
Dilum Aluthge <dilum@aluthge.com>
2020-08-05 08:23:31 -0400

    Add some more tests (#6)

--- test/unit-tests.jl
+++ test/unit-tests.jl
@@ -12,3 +12,7 @@ end
     @test DICOMClient._add_trailing_slash(HTTP.URI("https://juliahealth.org")) == HTTP.URI("https://juliahealth.org/")
     @test DICOMClient._add_trailing_slash(HTTP.URI("https://juliahealth.org/")) == HTTP.URI("https://juliahealth.org/")
 end
+
+@testset "parse_multipart_image" begin
+    @test_throws ArgumentError DICOMClient.parse_multipart_image(Val(:jpeg), UInt8[0xff, 0xd8, 0xff, 0xd9, 0xff, 0xd8])
+end
